d5a0804c66e229709bf2c4c4f62207464a3006ca
moritzgubler <moritz.gubler@gmail.com>
2022-11-22 10:47:41 +0100

    added leave stripping mechanism

--- src/python/graph.py
+++ src/python/graph.py
@@ -17,6 +17,12 @@ class MinimaHoppingGraph:
         self.graph = None
 
     def __enter__(self):
+        return self.read_from_disk()
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.write_to_disk()
+
+    def read_from_disk(self):
         if self.restart:
             graph_pickle = open(self.graphFileName, "rb")
             self.graph = pickle.load(graph_pickle)
@@ -26,12 +32,11 @@ class MinimaHoppingGraph:
             try:
                 os.remove(self.trajectoryDatabaseName)
             except FileNotFoundError:
-                #print('File not found')
                 pass
         self.trajectoryDict = shelve.open(self.trajectoryDatabaseName,  writeback=True)
         return self
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
+    def write_to_disk(self):
         self.trajectoryDict.close()
         graph_pickle = open (self.graphFileName, "wb")
         pickle.dump(self.graph, graph_pickle)
@@ -64,7 +69,36 @@ class MinimaHoppingGraph:
             self._add_edge(initialStuctureLabel, structureLabel, weight=weight_old, trajectory=mt)
             self._add_edge(structureLabel, initialStuctureLabel, weight=weight_new, trajectory=reverse_trajectory)
 
+    def get_lowest_energy(self):
+        emin = float("inf")
+        ind = -1
+        for n in self.graph.nodes:
+            e = float(self.graph.nodes[n]['energy'])
+            if emin > float(e):
+                emin = e
+                ind = n
+        self.graph.nodes[ind]['color'] = 'red'
+        return emin, ind
 
+    def shift_energy_to_zero(self):
+        emin, ind = self.get_lowest_energy()
+        for n in self.graph.nodes:
+            self.graph.nodes[n]['shifted_energy'] = float(self.graph.nodes[n]['energy']) - emin
+
+    def remove_leaves(self, number_of_iterations=1):
+        graph_copy = copy.deepcopy(self.graph)
+        nx.set_node_attributes(graph_copy, 0.5, 'width')
+        nx.set_node_attributes(graph_copy, 0.5, 'height')
+        for i in range(number_of_iterations):
+            remove = [node for node, degree in graph_copy.degree() if degree <= 2]
+            for i in remove:
+                for v in graph_copy.neighbors(i):
+                    # print(i, graph_copy.edges(i), v)
+                    graph_copy.nodes[v]['width'] = graph_copy.nodes[v]['width'] + 0.05
+                    graph_copy.nodes[v]['height'] = graph_copy.nodes[v]['height'] + 0.05
+            graph_copy.remove_nodes_from(remove)
+        return graph_copy
+    
     def _add_edge(self, initialStuctureLabel, structureLabel, weight, trajectory):
         self.graph.add_edge(initialStuctureLabel, structureLabel, weight=weight)
         self.trajectoryDict.sync()
--- src/python/parse_output.py
+++ src/python/parse_output.py
@@ -8,68 +8,36 @@ import pygraphviz
 
 import graph
 
-with graph.MinimaHoppingGraph('output/graph.dat', 'output/trajectory.dat', True) as g:
-    # g.draw()
-    n = 40
-    l = g.shortestPath(0, n)
-    tl = g.getTrajectoryList(0, n)
 
-    print(l)
+def draw_pygraphviz(g, filename, layout='fdp'):
+    g.graph_attr['concentrate'] = 'true'
+    # g.graph_attr['beautify'] = 'true'
+    g.layout(layout)
+    g.draw(filename)
+    # g.write('file.dot')
+    
 
-    f = open('output/good_trajectory.extxyz', 'w')
-    f.close()
+g = graph.MinimaHoppingGraph('output/graph.dat', 'output/trajectory.dat', True)
+g.read_from_disk()
+n = 28
+l = g.shortestPath(0, n)
+tl = g.getTrajectoryList(0, n)
 
-    write('output/good_trajectory.extxyz', tl, append = True)
+print(l)
 
+f = open('output/good_trajectory.extxyz', 'w')
+f.close()
 
-    graph = nx.nx_agraph.to_agraph(g.graph)
+write('output/good_trajectory.extxyz', tl, append = True)
 
-    graph.nodes()
+emin, ind = g.get_lowest_energy()
 
-    emin = 10000.0
-    ind = -1
+g.shift_energy_to_zero()
 
-    for n in graph.nodes():
-        try:
-            e = float(n.attr['energy'])
-            #print(e)
-            if e == '':
-                e = 0.0
-            if emin > float(e):
-                emin = float(n.attr['energy'])
-                ind = n.get_name()
-        except:
-            pass
+stripped_graph = g.remove_leaves()
 
-    graph.get_node(ind).attr['color'] = 'red'
-    graph.get_node(ind).attr['root'] = 'true'
+draw_pygraphviz(nx.nx_agraph.to_agraph(stripped_graph), 'no_leaves.pdf')
+draw_pygraphviz(nx.nx_agraph.to_agraph(g.graph), 'with_leaves.pdf')
 
+# graph.write('file.dot')
 
-    # e0 = emin + .8
-
-    # for n in graph.nodes():
-    #     graph.get_node(n.get_name()).attr['width'] = '.5'
-    #     graph.get_node(n.get_name()).attr['heigt'] = '.5'
-    #     try:
-    #         e = float(n.attr['energy'])
-    #         if e > e0:
-    #             ind1 = n.get_name()
-    #             graph.remove_node(ind1)
-    #     except:
-    #         pass
-
-
-    graph.graph_attr['concentrate'] = 'true'
-    #graph.graph_attr['K'] = 0.1
-    #graph.graph_attr['repulsiveforce'] = 2.0
-    #graph.graph_attr['beautify'] = 'true'
-    graph.layout('fdp')
-    graph.draw('networkx_graph.pdf')
-    graph.write('file.dot')
-
-
-    #graph = nx.drawing.nx_pydot.to_pydot(g.graph)
-    #graph.set_strict(True)
-    #graph.concentrate = True
-    #print(graph.get_strict(True))
-    #graph.write_png('output.pdf')
