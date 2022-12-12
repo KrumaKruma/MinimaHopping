f4b9d79663ca274cb74eee8f866e335934a921e2
moritzgubler <moritz.gubler@gmail.com>
2022-12-12 18:11:11 +0100

    mpi database working

--- minimahopping/MPI_database/mpi_database_worker.py
+++ minimahopping/MPI_database/mpi_database_worker.py
@@ -1,5 +1,6 @@
 import minimahopping.mh.minimum as minimum
 from mpi4py import MPI
+import minimahopping.MPI_database.mpi_messages as message
 
 
 class Database():
@@ -21,17 +22,27 @@ class Database():
         pass
 
     def addElement(self,struct: minimum.Minimum):
-        self.comm_world.send(minimum, dest=0)
+        minimum_copy = struct.__copy__()
+        self.comm_world.send((message.addelement, minimum_copy), dest=0)
         n_visit,label = self.comm_world.recv(source=0)
         struct.n_visit = n_visit
         struct.label = label
-        pass
+        return n_visit, label
 
     def addElementandConnectGraph(self, currentMinimum: minimum.Minimum, escapedMinimum: minimum.Minimum, trajectory, epot_max):
-        pass
+        self.comm_world.send((message.addElementandConnectGraph, [currentMinimum.__copy__(), escapedMinimum.__copy__(), trajectory, epot_max]), dest=0)
+        n_visit,label = self.comm_world.recv(source=0)
+        escapedMinimum.n_visit = n_visit
+        escapedMinimum.label = label
+        return n_visit, label
 
-    def get_element(self, struct: minimum.Minimum):
-        index = 0
-        return index
+    def get_element(self, index: int):
+        self.comm_world.send((message.get_element, index), dest=0)
+        element = self.comm_world.recv(source=0)
+        return element
+
+    def get_element_index(self, structure: minimum.Minimum):
+        self.comm_world.send((message.get_element_index, structure.__copy__()), dest = 0)
+        return self.comm_world.recv(source = 0)
 
 
---
+++ minimahopping/MPI_database/mpi_messages.py
@@ -0,0 +1,4 @@
+addelement = 'addElement'
+addElementandConnectGraph = 'addElementandConnectGraph'
+get_element = 'get_element'
+get_element_index = 'get_element_index'
