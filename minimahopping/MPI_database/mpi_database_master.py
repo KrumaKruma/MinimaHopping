0cab1f6818e20920e886f9669dce97d05f095ae5
moritzgubler <moritz.gubler@gmail.com>
2023-01-26 13:55:41 +0100

    improved efficiency output file io in server

--- minimahopping/MPI_database/mpi_database_master.py
+++ minimahopping/MPI_database/mpi_database_master.py
@@ -26,46 +26,48 @@ def MPI_database_server_loop(energy_threshold, minima_threshold, output_n_lowest
         # this set contains the ranks of all clients that stopped working.
         # When the size of this set is totalWorkers the server can be stopped
         stoppedClients = set()
-        while True:
-            continueSimulation = time.time() - t_start < maxTimeSeconds
+        with open('efficiencz.txt') as efficiencyFile:
+            efficiencyFile.write('#server_efficiency, processing time, waiting time\n')
+            while True:
+                continueSimulation = time.time() - t_start < maxTimeSeconds
 
-            print('server_efficiency, processing time, waiting time:', process_time / (process_time + wait_time+1e-6), process_time, wait_time, file=open('efficiency.txt', mode='a'))
-            t2 = time.time()
-            process_time += t2 - t1
-            t1 = time.time()
-            message_tag, data = comm_world.recv(status=status)
-            t2 = time.time()
-            sender = status.Get_source()
-            wait_time += t2 - t1
+                efficiencyFile.write('%f %f %f %f %f \n'%(process_time / (process_time + wait_time+1e-6), process_time, wait_time))
+                t2 = time.time()
+                process_time += t2 - t1
+                t1 = time.time()
+                message_tag, data = comm_world.recv(status=status)
+                t2 = time.time()
+                sender = status.Get_source()
+                wait_time += t2 - t1
 
-            t1 = time.time()
+                t1 = time.time()
 
-            if message_tag == message.addelement:
-                n_visit, label = db.addElement(data)
-                comm_world.send((n_visit, label), sender)
-            elif message_tag == message.addElementandConnectGraph:
-                n_visit, label, temp = db.addElementandConnectGraph(*data)
-                comm_world.send((n_visit, label, continueSimulation), sender)
-                if not continueSimulation:
-                    if sender in stoppedClients:
-                        print('Server thinks client stopped working, but client is still running.')
-                        print("rank of bad client:", sender)
-                        return
-                    stoppedClients.add(sender)
-                    current_workers = current_workers - 1
-            elif message_tag == message.get_element_index:
-                index = db.get_element_index(data)
-                comm_world.send(index, sender)
-            elif message_tag == message.get_element:
-                requested_minimum = db.get_element(data)
-                comm_world.send(requested_minimum, sender)
-            elif message_tag == message.clientWorkDone:
-                if not sender in stoppedClients: # client stopped running and was not stopped by server.
-                    stoppedClients.add(sender)
-                    current_workers = current_workers - 1
-            else:
-                print('tag not known, shutting down')
-                return
+                if message_tag == message.addelement:
+                    n_visit, label = db.addElement(data)
+                    comm_world.send((n_visit, label), sender)
+                elif message_tag == message.addElementandConnectGraph:
+                    n_visit, label, temp = db.addElementandConnectGraph(*data)
+                    comm_world.send((n_visit, label, continueSimulation), sender)
+                    if not continueSimulation:
+                        if sender in stoppedClients:
+                            print('Server thinks client stopped working, but client is still running.')
+                            print("rank of bad client:", sender)
+                            return
+                        stoppedClients.add(sender)
+                        current_workers = current_workers - 1
+                elif message_tag == message.get_element_index:
+                    index = db.get_element_index(data)
+                    comm_world.send(index, sender)
+                elif message_tag == message.get_element:
+                    requested_minimum = db.get_element(data)
+                    comm_world.send(requested_minimum, sender)
+                elif message_tag == message.clientWorkDone:
+                    if not sender in stoppedClients: # client stopped running and was not stopped by server.
+                        stoppedClients.add(sender)
+                        current_workers = current_workers - 1
+                else:
+                    print('tag not known, shutting down')
+                    return
 
-            if current_workers <= 0: # Last process that is still alive. The simulation can be stopped.
-                return
+                if current_workers <= 0: # Last process that is still alive. The simulation can be stopped.
+                    return
