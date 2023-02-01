84ec809ece9525f861134ab8e68f6a41223a222b
Moritz Gubler <moritz.gubler@gmail.com>
2023-02-01 17:41:59 +0100

    improved default values of parameters

--- minimahopping/mh/parameters.py
+++ minimahopping/mh/parameters.py
@@ -19,17 +19,17 @@ class minimaHoppingParameters:
     n_S_orbitals: int = 1                             # number of s orbitals for constructing the OMFP
     n_P_orbitals: int = 1                             # number of p orbitals for constructing the OMFP 
     width_cutoff: int = 3.5                           # cutoff for the OMFP
-    maxnatsphere: int = 100                           # Truncation of the OMFP length 
+    maxnatsphere: int = 50                           # Truncation of the OMFP length 
     exclude: list = field(default_factory=list)       # List of elements to exclude in the OMFP
     dt0: float = 0.01                                 # Initial dt for the MD
     _dt: float = -1.0                                 # dt of the MD for restart (if -1 then last dt is read)
     mdmin: int = 2                                    # number of minima visited before stopping the MD
     collect_md_data: bool = False                     # flag to collect MD data which later could be used e.g. in machine learning
-    fmax: float = 0.001                               # optimization stop criterion 
+    fmax: float = 0.01                               # optimization stop criterion 
     enhanced_feedback:bool = False                    # enhanced feedback (rise temperature according to T = T * beta_increase * (1. + 1. * ln(n_visits)))
-    energy_threshold: float = 0.0001                  # if the energy difference of two structures is below the fingerprint is compared
+    energy_threshold: float = 0.01                    # if the energy difference of two structures is below the fingerprint is compared
     output_n_lowest_minima: int = 20                  # outputs the n lowest minima
-    fingerprint_threshold: float = 1e-3               # OMFP distance threshold for distinguishing minima
+    fingerprint_threshold: float = 5e-3               # OMFP distance threshold for distinguishing minima
     verbose_output: bool = False                      # if True MD and OPT logs are written
     new_start: bool = False                           
     run_time: str = "infinite"                        # runtime in the format (d-hh:mm:ss) or inifinite for infinite runtime
