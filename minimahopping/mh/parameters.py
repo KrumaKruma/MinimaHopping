f372ae23604f0cced1d7f9eed9b4b3fd6cac1f23
moritzgubler <moritz.gubler@gmail.com>
2023-02-03 13:59:13 +0100

    corrected documentation in parameters

--- minimahopping/mh/parameters.py
+++ minimahopping/mh/parameters.py
@@ -5,41 +5,77 @@ import logging
 @dataclass_json
 @dataclass
 class minimaHoppingParameters:
-    T0: float = 1000                                  # Initial temperature to start MH
-    _T: float = -1.0                                  # Temperature for restart (if -1 then last temperature is read)
-    _eDiff: float = -1.0                              # Ediff for restart (if -1 then last Ediff is read)
-    Ediff0: float = .1                                # Initital Ediff
-    beta_decrease: float = 1./1.02                    # factor for decreaseing the temperature
-    beta_increase: float = 1.02                       # factor for increaseing the temperature
-    alpha_accept: float = 1/1.02                      # factor for decreaseing Ediff
-    alpha_reject: float = 1.02                        # factor for increaseing Ediff
-    n_soft: int = 20                                  # number of softening steps
-    soften_positions: float = 1e-2                    # step size for softening the positions
-    soften_lattice: float = 1e-3                      # step size for softening the lattice (only relevant for periodic boundary conditions)
-    n_S_orbitals: int = 1                             # number of s orbitals for constructing the OMFP
-    n_P_orbitals: int = 1                             # number of p orbitals for constructing the OMFP 
-    width_cutoff: int = 3.5                           # cutoff for the OMFP
-    maxnatsphere: int = 50                           # Truncation of the OMFP length 
-    exclude: list = field(default_factory=list)       # List of elements to exclude in the OMFP
-    dt0: float = 0.01                                 # Initial dt for the MD
-    _dt: float = -1.0                                 # dt of the MD for restart (if -1 then last dt is read)
-    mdmin: int = 2                                    # number of minima visited before stopping the MD
-    collect_md_data: bool = False                     # flag to collect MD data which later could be used e.g. in machine learning
-    fmax: float = 0.01                               # optimization stop criterion 
-    enhanced_feedback:bool = False                    # enhanced feedback (rise temperature according to T = T * beta_increase * (1. + 1. * ln(n_visits)))
-    energy_threshold: float = 0.01                    # if the energy difference of two structures is below the fingerprint is compared
-    output_n_lowest_minima: int = 20                  # outputs the n lowest minima
-    fingerprint_threshold: float = 5e-3               # OMFP distance threshold for distinguishing minima
-    verbose_output: bool = False                      # if True MD and OPT logs are written
-    new_start: bool = False                           
-    run_time: str = "infinite"                        # runtime in the format (d-hh:mm:ss) or inifinite for infinite runtime
-    use_intermediate_mechanism: bool = False          
-    write_graph_output: bool = True                    
+    T0: float = 1000
+    """T0 (float): Initial temperature to start MH"""
+    _T: float = -1.0
+    """"_T (float): Temperature for restart (if -1 then last temperature is read)"""
+    Ediff0: float = .1
+    """"Ediff0 (float): Initital energy difference for accepting minima"""
+    _eDiff: float = -1.0
+    """"_eDiff, Private (float): Energy difference for accepting minima after restarts."""
+    beta_decrease: float = 1./1.02
+    """beta_decrease (float): factor for decreaseing the temperature"""
+    beta_increase: float = 1.02
+    """beta_increase (float): Factor for increaseing the temperature"""
+    alpha_accept: float = 1/1.02
+    """alpha_accept (float): Factor for increaseing Ediff"""
+    alpha_reject: float = 1.02
+    """alpha_reject (float): Factor for decreaseing Ediff"""
+    n_soft: int = 20
+    """n_soft (int): number of softening steps"""
+    soften_positions: float = 1e-2
+    """soften_positions (float): step size for softening the positions"""
+    soften_lattice: float = 1e-3
+    """soften_lattice (float): step size for softening the lattice (only relevant for periodic boundary conditions)"""
+    n_S_orbitals: int = 1
+    """n_S_orbitals (int): number of s orbitals for constructing the OMFP"""
+    n_P_orbitals: int = 1
+    """n_P_orbitals (int): number of p orbitals for constructing the OMFP"""
+    width_cutoff: int = 3.5
+    """width_cutoff (float): cutoff for the OMFP"""
+    maxnatsphere: int = 50
+    """maxnatsphere (int): Truncation lentgh of the OMFP length"""
+    exclude: list = field(default_factory=list)
+    """exclude (list): List of elements to exclude in the OMFP"""
+    dt0: float = 0.01
+    """dt0 (float): Initial time step for the MD"""
+    _dt: float = -1.0
+    """_dt, Private (float): Time step of the MD for restart."""
+    mdmin: int = 2
+    """mdmin (int): Number of minima visited before stopping the MD"""
+    collect_md_data: bool = False
+    """collect_md_data (bool): flag to collect MD data which later could be used e.g. in machine learning"""
+    fmax: float = 0.01
+    """fmax (float): Maximal force component. Used as a stopping criterion for the geometry optimization."""
+    enhanced_feedback:bool = False
+    """enhanced_feedback (bool):Enhanced feedback (rise temperature according to T = T * beta_increase * (1. + 1. * ln(n_visits))). Unclear if this improves performance"""
+    energy_threshold: float = 0.01
+    """energy_threshold (float): if the energy difference of two structures is below the fingerprint is compared"""
+    output_n_lowest_minima: int = 20
+    """output_n_lowest_minima (int): Outputs the n lowest minima"""
+    fingerprint_threshold: float = 5e-3
+    """fingerprint_threshold (float): OMFP distance threshold for distinguishing minima"""
+    verbose_output: bool = False
+    """verbose_output (bool): If True MD and OPT logs are written."""
+    new_start: bool = False
+    """new_start (bool): Start from scratch even if restart files are present (deporecated)"""
+    run_time: str = "infinite"
+    """run-time (str): Runtime in the format (d-hh:mm:ss) or inifinite for infinite runtime."""
+    use_intermediate_mechanism: bool = False
+    """use_intermediate_mechanism (bool): Sets if intermediate minimas will be stored and accepted if necessary."""
+    write_graph_output: bool = True
+    """write_graph_output (bool): Determines wether graph is calculated and written to file. Creates rather large files when hundred of thousands structures are found."""
     use_MPI: bool = False
+    """use_MPI (bool): Sets if MPI with a master client model should be used"""
     logLevel: int = logging.INFO
+    """logLevel (int): Sets the logging level."""
     _n_accepted: int = 0
+    """_n_accepted (int): Private counting variable for number of accepted minima."""
     _n_rejected: int = 0
+    """_n_rejected (int): Private counting variable for number of rejected minima."""
     _n_same: int = 0
+    """_n_same (int): Private conting variable for number of unsuccessful escape attempts."""
+
 
     def getFixedParameterList(self):
         return ['n_S_orbitals', 'n_P_orbitals', 'width_cutoff', 'exclude', 'fingerprint_threshold', 'use_MPI', 'maxnatsphere']
