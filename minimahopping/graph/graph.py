a1d3f9aaa5487dcfc74c2f829fe53a3550d5c988
moritzgubler <moritz.gubler@gmail.com>
2023-07-26 15:54:45 +0200

    create colorbar when graph is colored

--- minimahopping/graph/graph.py
+++ minimahopping/graph/graph.py
@@ -3,10 +3,12 @@ import numpy as np
 import shelve
 import pickle
 import os
-import matplotlib.pyplot as plt
 from ase import Atoms
 import copy
+from matplotlib import rcParams
+rcParams['font.family'] = 'Times New Roman'
 import matplotlib
+import matplotlib.pyplot as plt
 
 graphDotName = 'output/graph.dot'
 
@@ -158,7 +160,23 @@ def color_graph(graph: nx.DiGraph):
     nx.set_node_attributes(graph, 'blue', 'fillcolor')
     nx.set_node_attributes(graph, 'filled', 'style')
     for n in graph.nodes():
-        graph.nodes()[n]['fillcolor'] = matplotlib.colors.rgb2hex(cmap(graph.nodes()[n]['shifted_energy'] / graph.nodes()[n]['num_atoms'] ))
+        intensity = graph.nodes()[n]['shifted_energy'] / graph.nodes()[n]['num_atoms']
+        intensity = intensity * 10
+        graph.nodes()[n]['fillcolor'] = matplotlib.colors.rgb2hex(cmap( intensity ))
+
+    # todo: create a colorbar pdf
+    cm = 1/2.54
+    gradient = np.linspace(0, 1, 101, endpoint=True)
+    gradient = np.vstack((gradient, gradient))
+    fig, axs = plt.subplots(figsize=( 6.8 * cm, 2.8* cm))
+    # fig, axs = plt.subplots()
+    # fig.set_size_inches(6*cm, 5*cm)
+    axs.set_xlabel('meV per Atom', fontsize=10)
+    plt.yticks([])
+    plt.xticks([0, 20, 40, 60, 80, 100])
+    axs.imshow(gradient, aspect=7.0, cmap=cmap)
+    # plt.show()
+    fig.savefig('coloourbar.pdf')
 
 def remove_leaves_static(graph, number_of_iterations: int = 1):
     graph_copy = copy.deepcopy(graph)
