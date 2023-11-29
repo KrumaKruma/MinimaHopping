6bce1d798c6d9a5eeac519bea33583a060d903d9
Marco Krummenacher <marco.krummenacher@unibas.ch>
2023-11-29 11:25:49 +0100

    clean up slab examples

--- example/slab/fixed_cell/fixed_cell.py
+++ example/slab/fixed_cell/fixed_cell.py
@@ -3,16 +3,24 @@ from ase.build import fcc110
 from ase.calculators.emt import EMT
 from minimahopping.minhop import Minimahopping
 
-# Make the Pt 110 slab.
-atoms = fcc110('Pt', (2, 2, 2), vacuum=7.)
 
-# Add the Cu2 adsorbate.
-adsorbate = Atoms([Atom('Cu', atoms[7].position + (0., 0., 2.5)),
-                   Atom('Cu', atoms[7].position + (0., 0., 5.0))])
-atoms.extend(adsorbate)
+def main():
 
-# Set the calculator.
-atoms.calc = EMT()
+    # Make the Pt 110 slab.
+    initial_configuration = fcc110('Pt', (2, 2, 2), vacuum=7.)
 
-with Minimahopping(atoms, mdmin=3, fixed_cell_simulation = True,verbose_output=True, T0=500, dt0=0.01, use_MPI=False) as mh:
-    mh(totalsteps=50)
+    # Add the Cu2 adsorbate.
+    adsorbate = Atoms([Atom('Cu', initial_configuration[7].position + (0., 0., 2.5)),
+                   Atom('Cu', initial_configuration[7].position + (0., 0., 5.0))])
+    initial_configuration.extend(adsorbate)
+
+    # Set the calculator.
+    initial_configuration.calc = EMT()
+
+    with Minimahopping(initial_configuration, mdmin=3, fixed_cell_simulation = True,verbose_output=True, T0=500, dt0=0.01, use_MPI=False) as mh:
+        mh(totalsteps=50)
+
+
+if __name__ == '__main__':
+    main()
+    quit()
