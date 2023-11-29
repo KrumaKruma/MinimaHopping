8724b49bc3c587bf30c74fcd3c93722251538494
Marco Krummenacher <marco.krummenacher@unibas.ch>
2023-11-29 14:49:37 +0100

    function for periodicity check

--- minimahopping/mh/lattice_operations.py
+++ minimahopping/mh/lattice_operations.py
@@ -34,3 +34,38 @@ def reshape_cell(atoms: Atoms, symprec: float):
         atoms.set_scaled_positions(scaled_positions)
 
 
+def check_boundary_conditions(atoms):
+    """
+    Function that returns checks and returns the type periodic boundary conditions.
+    0: non-periodic/cluster simulation
+    1: periodicity in one direction (error not implemented)
+    2: 2-d periodic boundaries check if initialized correctly
+    3: fully periodic simulation
+    """
+    
+    number_of_periodic_axes = sum(atoms.pbc)
+
+    if number_of_periodic_axes == 0:
+        boundary_type = 0
+    elif number_of_periodic_axes == 1:
+        # 1-dimensional pbc is not implemented
+        logging.logger.info("1-D boundary conditions are not implemented")
+        quit()
+    elif number_of_periodic_axes == 2:
+        # check if lattice is correct
+        index = np.where(atoms.pbc==False)[0]
+        sum_offset_zcell = np.sum(atoms.cell[2,:2] + atoms.cell[:2,2])
+        if sum_offset_zcell > 1e-10:
+            logging.logger.info("Lattice is not correctly adjusted so that variable cell shape slab can be used.")
+            quit()
+        elif index != 2:
+            logging.logger.info("Abort simulation: For slab simulations z-dimension has to be non-periodic")
+            quit()
+        else:
+            boundary_type = 2
+    elif number_of_periodic_axes == 3:
+        boundary_type = 3
+
+    return boundary_type
+
+
