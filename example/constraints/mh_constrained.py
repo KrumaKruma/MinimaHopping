586575e0e9a3b8342d39904ca99ace5caa65f3cc
Marco Krummenacher <marco.krummenacher@unibas.ch>
2023-11-29 11:19:04 +0100

    clean up constraint example

--- example/constraints/mh_constrained.py
+++ example/constraints/mh_constrained.py
@@ -5,18 +5,21 @@ from ase.constraints import FixAtoms
 
 
 def main():
-    atoms = wulff_construction('Na', surfaces=[(1, 0, 0), (0, 1, 0),(0, 0, 1)], energies=[0.001, 0.001, 0.15],
+    # construct a chain with 13 atoms:
+    initial_configuration = wulff_construction('Na', surfaces=[(1, 0, 0), (0, 1, 0),(0, 0, 1)], energies=[0.001, 0.001, 0.15],
                             size=13, # maximum number of atoms
                             structure='bcc', rounding='above')
 
-
+    # set up eam calculator and attach it to atoms object
     calculator = EAM(potential="Na_v2.eam.fs")
-    atoms.calc = calculator
+    initial_configuration.calc = calculator
+
+    # set up constraints and fixing atom 0 and 1
     constraints = [FixAtoms(indices=[0,1])]
 
-    atoms.calc = calculator
+    initial_configuration.calc = calculator
 
-    with Minimahopping(atoms, mdmin=2, constraints=constraints,initial_step_size=1e-3,verbose_output=True, T0=1000, dt0=0.1, use_MPI=False) as mh:
+    with Minimahopping(initial_configuration, mdmin=2, constraints=constraints,initial_step_size=1e-3,verbose_output=True, T0=1000, dt0=0.1, use_MPI=False) as mh:
         mh(totalsteps=50)
 
 if __name__ == '__main__':
