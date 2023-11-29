8e12d9a3b6f9254a2d29c718b827c32fe69e874a
Marco Krummenacher <marco.krummenacher@unibas.ch>
2023-11-29 11:21:41 +0100

    clean up the md calculator example

--- example/md_calculator/mh_si4_md_calculator.py
+++ example/md_calculator/mh_si4_md_calculator.py
@@ -8,11 +8,13 @@ from ase.build import bulk
 from ase.io import read, write
 
 def main():
-    #a0 = 3.#(15.0*4)**(1.0/3.0)
-    #initial_configuration = bulk('Si')#, a=3.6, crystalstructure='fcc')
-    #print(initial_configuration)
+    
     initial_configuration = read("input.extxyz")
+
+    # set the psuedo potential for quantum espresso
     pseudopotentials = {'Si': 'Si.pbe-nl-rrkjus_psl.1.0.0.UPF'}
+    
+    # set up the quantum espressoi calculator
     input_data = {
         'system': {
             'ecutwfc': 60.,
@@ -27,10 +29,12 @@ def main():
         'rsim':{
             'smear3d': 2.}}
 
-    md_calculator = KIM("SW_StillingerWeber_1985_Si__MO_405512056662_005")
     
     calculator = Espresso(pseudopotentials=pseudopotentials,
                 tstress=True, tprnfor=True, kpts=(3, 3, 3), koffset=(0, 0, 0),input_data=input_data)
+    
+    # set up the kim calculator for md and pre optimization
+    md_calculator = KIM("SW_StillingerWeber_1985_Si__MO_405512056662_005")
 
     initial_configuration.calc = calculator
     with Minimahopping(initial_configuration, md_calculator=md_calculator, mdmin=2,verbose_output=True, T0=4000, dt0=0.01, use_MPI=False) as mh:
