553c9d3cd22dce6357a806a0ef8f34caac807dff
moritzgubler <moritz.gubler@gmail.com>
2023-12-18 15:24:47 +0100

    update enthalpy calculator

--- minimahopping/pressureCalculator/pressureTimesVolumeCalculator.py
+++ minimahopping/pressureCalculator/pressureTimesVolumeCalculator.py
@@ -5,22 +5,20 @@ from ase import units
 
 class EnthalpyCalculator(Calculator):
     """
-    Calculates p*V and returns it as the potential energy. The stress is also adjusted. Forces are also implemented.
-    There, the calculator will always return zero.
-    This calculator is meant to be used as part of the SumCalculator from ase. A normal calculator calulator 
-    can be transformed into an enthalpy calculator the following way:
-    >>> atoms.calc = ase.calculators.mixing.SumCalculator([normalCalculator, EnthalpyCalculator(pressure_giga_pascale = 5)])
+    Calculates p*V and adds it to the potential energy of the base calculator. The stress is also adjusted. Forces are also implemented.
+    >>> atoms.calc = EnthalpyCalculator(normalCalculator, pressure_giga_pascale = 5)
     The new calculator will now calculate H=E+pV and adjust the stress accordingly.
     """
     implemented_properties = ['energy', 'forces', 'stress']
     default_parameters = {}
     nolabel = True
 
-    def __init__(self, pressure_giga_pascale: float = 0.0):
-
+    def __init__(self, base_calculator: Calculator, pressure_giga_pascale: float = 0.0):
         super().__init__()
+        self.base_calculator = base_calculator
         # self.pressure is in ase units.
         self.pressure = pressure_giga_pascale * units.GPa
+        
 
     def calculate(
         self,
@@ -39,14 +37,14 @@ class EnthalpyCalculator(Calculator):
             if 'stress' in properties:
                 self.results['stress'] = np.zeros(6)
             if 'energy' in properties:
-                self.results['energy'] = 0.0
+                self.results['energy'] = self.base_calculator.get_potential_energy(atoms)
             if 'forces' in properties:
-                self.results['forces'] = np.zeros((len(atoms), 3))
+                self.results['forces'] = self.base_calculator.get_forces(atoms)
 
         else:
             if 'stress' in properties:
-                self.results['stress'] = np.array([self.pressure, self.pressure, self.pressure, .0, .0, .0])
+                self.results['stress'] = self.base_calculator.get_stress(atoms) + np.array([self.pressure, self.pressure, self.pressure, .0, .0, .0])
             if 'energy' in properties:
-                self.results['energy'] = self.pressure * atoms.get_volume()
+                self.results['energy'] = self.base_calculator.get_potential_energy(atoms) +  self.pressure * atoms.get_volume()
             if 'forces' in properties:
-                self.results['forces'] = np.zeros((len(atoms), 3))
+                self.results['forces'] = self.base_calculator.get_forces(atoms)
