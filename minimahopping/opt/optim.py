61f1aee3a2dd511c37561e81998743493a4052a7
Moritz Gubler <moritz.gubler@gmail.com>
2024-06-14 15:48:01 +0000

    Revert "Merge branch 'revert-e657ac98' into 'main'"
    This reverts merge request !9

--- minimahopping/opt/optim.py
+++ minimahopping/opt/optim.py
@@ -4,9 +4,21 @@ from ase.io import write
 import minimahopping.mh.lattice_operations as lattice_operations
 from sqnm.vcsqnm_for_ase import aseOptimizer
 import minimahopping.md.md
+import ase.atom
+import typing
 
 
-def optimization(atoms, calculator, max_force_threshold, outpath, fixed_cell_simulation=False, initial_step_size=None, nhist_max=10, lattice_weight=2, alpha_min=1e-3, eps_subsp=1e-3, verbose=True):
+def optimization(atoms: ase.atom.Atom, 
+                 calculator: ase.calculators.calculator.Calculator, 
+                 max_force_threshold: float, 
+                 outpath: str, 
+                 fixed_cell_simulation: bool = False, 
+                 initial_step_size: float = None, 
+                 nhist_max: int = 10, 
+                 lattice_weight:float = 2, 
+                 alpha_min: float = 1e-3, 
+                 eps_subsp: float = 1e-3, 
+                 verbose: bool = True):
     # copy the atoms object and attach calculator to it
     atoms = atoms.copy()
     atoms.calc = calculator
@@ -14,7 +26,7 @@ def optimization(atoms, calculator, max_force_threshold, outpath, fixed_cell_sim
     # If verbose then open the files for writing
     if verbose:
         optimization_trajectory_file = open(outpath + "geometry_optimization_trajectory.extxyz", "w")
-        write(optimization_trajectory_file, atoms, parallel=False)
+        write(filename = optimization_trajectory_file, images = atoms, parallel=False)
         optimization_log_file = open(outpath + "geometry_optimization_log.dat", "w")
         msg = 'STEP      ETOT              MAX_FORCE       GAIN_RATIO       STEPSIZE           DIM_SUPSP         MAX_DISP\n'
         optimization_log_file.write(msg)
@@ -24,17 +36,17 @@ def optimization(atoms, calculator, max_force_threshold, outpath, fixed_cell_sim
 
     try:
         # Run geometry optimization
-        trajectory, optimizer, number_of_steps, epot_max = geometry_optimization(atoms,
-                                                                    fixed_cell_simulation, 
-                                                                    max_force_threshold, 
-                                                                    initial_step_size, 
-                                                                    nhist_max, 
-                                                                    lattice_weight, 
-                                                                    alpha_min, 
-                                                                    eps_subsp, 
-                                                                    verbose, 
-                                                                    optimization_trajectory_file,
-                                                                    optimization_log_file)
+        trajectory, optimizer, number_of_steps, epot_max = geometry_optimization(atoms = atoms,
+                                                                    fixed_cell_simulation = fixed_cell_simulation, 
+                                                                    max_force_threshold = max_force_threshold, 
+                                                                    initial_step_size = initial_step_size, 
+                                                                    nhist_max = nhist_max, 
+                                                                    lattice_weight = lattice_weight, 
+                                                                    alpha_min = alpha_min, 
+                                                                    eps_subsp = eps_subsp, 
+                                                                    verbose = verbose, 
+                                                                    optimization_trajectory_file = optimization_trajectory_file,
+                                                                    optimization_log_file = optimization_log_file)
         positions_out = atoms.get_positions()
         lattice_out = atoms.get_cell()
         noise = optimizer.optimizer.lower_bound()
@@ -47,7 +59,17 @@ def optimization(atoms, calculator, max_force_threshold, outpath, fixed_cell_sim
 
 
 
-def geometry_optimization(atoms, fixed_cell_simulation,max_force_threshold,initial_step_size, nhist_max, lattice_weight, alpha_min, eps_subsp, verbose, optimization_trajectory_file, optimization_log_file):
+def geometry_optimization(atoms: ase.atom.Atom, 
+                          fixed_cell_simulation: bool,
+                          max_force_threshold: float,
+                          initial_step_size: float, 
+                          nhist_max: int, 
+                          lattice_weight: float, 
+                          alpha_min: float, 
+                          eps_subsp: float, 
+                          verbose: bool, 
+                          optimization_trajectory_file: typing.IO, 
+                          optimization_log_file: typing.IO):
     # check if periodic boundary condition and assert that either fully periodic or non-periodic
     '''
     geometry optimization
@@ -72,7 +94,7 @@ def geometry_optimization(atoms, fixed_cell_simulation,max_force_threshold,initi
         initial_step_size = -0.001
     
     # Set if free relax or vcs relax
-    periodic_type = lattice_operations.check_boundary_conditions(atoms)
+    periodic_type = lattice_operations.check_boundary_conditions(atoms = atoms)
     if periodic_type != 0 and not fixed_cell_simulation:
         vc_relax = True
     else:
@@ -91,7 +113,7 @@ def geometry_optimization(atoms, fixed_cell_simulation,max_force_threshold,initi
     while max_force_comp > max_force_threshold:
         pos_in = atoms.get_positions()
         
-        optimizer.step(atoms)
+        optimizer.step(atoms = atoms)
         max_force_comp = optimizer._getDerivativeNorm()
         
         i_step += 1
@@ -101,25 +123,39 @@ def geometry_optimization(atoms, fixed_cell_simulation,max_force_threshold,initi
             epot_max = energy
 
         if verbose:
-            write_log(atoms, optimizer, i_step, max_force_comp, max_disp, optimization_trajectory_file, optimization_log_file)
+            write_log(atoms = atoms, 
+                      optimizer = optimizer, 
+                      i_step = i_step, 
+                      max_force_comp = max_force_comp, 
+                      max_disp = max_disp, 
+                      optimization_trajectory_file = optimization_trajectory_file, 
+                      optimization_log_file = optimization_log_file)
         
-        positions_old, lattice_old, is_append_trajectory = minimahopping.md.md.check_coordinate_shift(atoms, positions_old, lattice_old)
+        positions_old, lattice_old, is_append_trajectory = minimahopping.md.md.check_coordinate_shift(atoms = atoms, 
+                                                                                                      positions_old = positions_old, 
+                                                                                                      lattice_old = lattice_old)
         if is_append_trajectory:
             trajectory.append(atoms.copy())
             trajectory[-1].info['energy'] = energy
             trajectory[-1].info.pop('label', None)
 
-        is_aboard = check(i_step)
+        is_aboard = check(i_step = i_step)
         if is_aboard:
             break
 
         pos_out = atoms.get_positions()
-        max_disp = get_max_disp(pos_in, pos_out)
+        max_disp = get_max_disp(pos_in = pos_in, pos_out = pos_out)
 
     return trajectory, optimizer, i_step, epot_max
 
 
-def write_log(atoms, optimizer, i_step, max_force_comp, max_disp, optimization_trajectory_file, optimization_log_file):
+def write_log(atoms: ase.atom.Atom, 
+              optimizer: aseOptimizer, 
+              i_step: int, 
+              max_force_comp: float, 
+              max_disp: float, 
+              optimization_trajectory_file: typing.IO, 
+              optimization_log_file: typing.IO):
     '''
     If verbose is True each optimization step is written to a file and energy and the max force component is
     printed
@@ -137,10 +173,10 @@ def write_log(atoms, optimizer, i_step, max_force_comp, max_disp, optimization_t
     forces = atoms.get_forces()
     optimization_log_file.write(opt_msg)
     optimization_log_file.flush()
-    write(optimization_trajectory_file, atoms, parallel=False)
+    write(filename = optimization_trajectory_file, images = atoms, parallel=False)
     optimization_trajectory_file.flush()
 
-def check(i_step):
+def check(i_step: int):
     '''
     Check if the geometry optimization has reached the limit of 10000 optimization steps.
     '''
@@ -153,7 +189,7 @@ def check(i_step):
     return is_aboard
 
 
-def get_max_disp(pos_in, pos_out):
+def get_max_disp(pos_in: np.ndarray, pos_out: np.ndarray):
     displacements = pos_in-pos_out
     max_disp = np.max(displacements)
     return max_disp
