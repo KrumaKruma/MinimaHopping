b952c42dcc1524eb8e3303eaeea45154b98ef30b
Cedric Hannemann <hannemann@fhi.mpg.de>
2024-08-14 14:05:00 +0000

    <docs>[parameters.py]: fix typos and inconsistent docstrings

--- minimahopping/mh/parameters.py
+++ minimahopping/mh/parameters.py
@@ -10,31 +10,31 @@ class minimaHoppingParameters:
     _T: float = -1.0
     """"_T (float): Temperature for restart (if -1 then last temperature is read)"""
     Ediff0: float = .1
-    """"Ediff0 (float): Initital energy difference for accepting minima"""
+    """"Ediff0 (float): Initial energy difference for accepting minima"""
     _eDiff: float = -1.0
     """"_eDiff, Private (float): Energy difference for accepting minima after restarts."""
     beta_decrease: float = 1./1.05
-    """beta_decrease (float): Factor for decreaseing the temperature"""
+    """beta_decrease (float): Factor for decreasing the temperature"""
     beta_increase: float = 1.05
-    """beta_increase (float): Factor for increaseing the temperature"""
+    """beta_increase (float): Factor for increasing the temperature"""
     alpha_accept: float = 1/1.05
     """alpha_accept (float): Factor for decreasing Ediff"""
     alpha_reject: float = 1.05
     """alpha_reject (float): Factor for increasing Ediff"""
     fixed_cell_simulation: bool = False
-    """fixed_cell_simulation(bool): if True the simulation cell is fixed and no variable cell shape MD and optimization are performed"""
+    """fixed_cell_simulation(bool): If True the simulation cell is fixed and no variable cell shape MD and optimization are performed"""
     n_soft: int = 20
-    """n_soft (int): number of softening steps"""
+    """n_soft (int): Number of softening steps"""
     soften_positions: float = 1e-3
-    """soften_positions (float): step size for softening the positions"""
+    """soften_positions (float): Step size for softening the positions"""
     soften_lattice: float = 1e-3
-    """soften_lattice (float): step size for softening the lattice (only relevant for periodic boundary conditions)"""
+    """soften_lattice (float): Step size for softening the lattice (only relevant for periodic boundary conditions)"""
     n_S_orbitals: int = 1
-    """n_S_orbitals (int): number of s orbitals for constructing the OMFP"""
+    """n_S_orbitals (int): Number of s orbitals for constructing the OMFP"""
     n_P_orbitals: int = 0
-    """n_P_orbitals (int): number of p orbitals for constructing the OMFP"""
+    """n_P_orbitals (int): Number of p orbitals for constructing the OMFP"""
     width_cutoff: int = 4
-    """width_cutoff (float): cutoff for the OMFP"""
+    """width_cutoff (float): Cutoff for the OMFP"""
     exclude: list = field(default_factory=list)
     """exclude (list): List of elements to exclude in the OMFP"""
     dt0: float = 0.08
@@ -42,35 +42,35 @@ class minimaHoppingParameters:
     _dt: float = -1.0
     """_dt, Private (float): Time step of the MD for restart."""
     dt_min: float = 0.0001
-    """dt_min, (float): minimal time step of the MD."""
+    """dt_min, (float): Minimal time step of the MD."""
     mdmin: int = 2
     """mdmin (int): Number of minima visited before stopping the MD"""
     md_max_steps: int = 10000
-    """md_max_steps (int): Maximum number of MD steps. If reached MH continous"""
+    """md_max_steps (int): Maximum number of MD steps. If reached MH continuous"""
     collect_md_data: bool = False
-    """collect_md_data (bool): flag to collect MD data which later could be used e.g. in machine learning"""
+    """collect_md_data (bool): Flag to collect MD data which later could be used e.g. in machine learning"""
     margin: float = 0.3
-    """margin for fixing of fragmentation, if particle closer than (2*max_rcov + 2*margin*max_rcov)"""
+    """margin (float): Margin for fixing of fragmentation, if particle closer than (2*max_rcov + 2*margin*max_rcov)"""
     symprec: float = 1e-5
     """symprec (float): Distance tolerance in Cartesian coordinates to find crystal symmetry for reshape cell operation (see spglib documentation for more info)"""
     fmax_pre_optimization : float = 0.1
-    """fmax_pre_optimization: Maximal force component. Used as stopping criterion for pre geometry optimization (only used if second calculator is aviable)"""
+    """fmax_pre_optimization (float): Maximal force component. Used as stopping criterion for pre geometry optimization (only used if second calculator is available)"""
     fmax: float = 0.01
     """fmax (float): Maximal force component. Used as a stopping criterion for the geometry optimization."""
     initial_step_size: float = None
-    """inital step size of the geometry optimizer. If None the initial step size is estimated"""
+    """initial_step_size (float): Initial step size of the geometry optimizer. If None the initial step size is estimated"""
     nhist_max: int = 10
-    """maximal length of history list in sqnm geometry optimizer"""
+    """nhist_max (int): Maximal length of history list in sqnm geometry optimizer"""
     lattice_weight: float = 2.
-    """weight / size of the supercell that is used to transform lattice derivatives. Use a value between 1 and 2. Default is 2."""
+    """lattice_weight (float): Weight / size of the supercell that is used to transform lattice derivatives. Use a value between 1 and 2. Default is 2."""
     alpha_min: float = 1e-3
-    """Lower limit on the step size. 1.e-2 is the default."""
+    """alpha_min (float): Lower limit on the step size. 1.e-2 is the default."""
     eps_subsp: float = 1e-3
-    """Lower limit on linear dependencies of basis vectors in history list."""
+    """esp_subsp (float): Lower limit on linear dependencies of basis vectors in history list."""
     enhanced_feedback:bool = False
-    """enhanced_feedback (bool):Enhanced feedback (rise temperature according to T = T * beta_increase * (1. + 1. * ln(n_visits)))."""
+    """enhanced_feedback (bool): Enhanced feedback (rise temperature according to T = T * beta_increase * (1. + 1. * ln(n_visits)))."""
     energy_threshold: float = 0.001
-    """energy_threshold (float): if the energy difference of two structures is below the fingerprint is compared"""
+    """energy_threshold (float): If the energy difference of two structures is below the fingerprint is compared"""
     output_n_lowest_minima: int = 20
     """output_n_lowest_minima (int): Outputs the n lowest minima"""
     fingerprint_threshold: float = 5e-2
@@ -78,9 +78,9 @@ class minimaHoppingParameters:
     verbose_output: bool = False
     """verbose_output (bool): If True MD and OPT logs are written."""
     new_start: bool = False
-    """new_start (bool): Start from scratch even if restart files are present (deporecated)"""
+    """new_start (bool): Start from scratch even if restart files are present (deprecated)"""
     run_time: str = "infinite"
-    """run-time (str): Runtime in the format (d-hh:mm:ss) or inifinite for infinite runtime."""
+    """run-time (str): Runtime in the format (d-hh:mm:ss) or infinite for infinite runtime."""
     use_intermediate_mechanism: bool = False
     """use_intermediate_mechanism (bool): Sets if intermediate minimas will be stored and accepted if necessary."""
     write_graph_output: bool = True
@@ -94,7 +94,7 @@ class minimaHoppingParameters:
     _n_rejected: int = 0
     """_n_rejected (int): Private counting variable for number of rejected minima."""
     _n_same: int = 0
-    """_n_same (int): Private conting variable for number of unsuccessful escape attempts."""
+    """_n_same (int): Private counting variable for number of unsuccessful escape attempts."""
     maxNumberOfMinima: int = 0
     """maxNumberOfMinima (int): Maximal number of minima that will be stored in database. Only the maxNumberOfMinima lowest energy structures will be stored. If a structure is higher in energy than the 
     maxNumberOfMinima it will considered as a new structure. If the number is 0 or negative, it will be considered as infinite."""
