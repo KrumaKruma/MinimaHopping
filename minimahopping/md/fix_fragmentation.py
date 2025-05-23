1c38bf21bcb7aeaa975ef19d6564fc4c8a6f6111
moritzgubler <moritz.gubler@gmail.com>
2025-05-23 10:51:48 +0200

    add slab implementation

--- minimahopping/md/fix_fragmentation.py
+++ minimahopping/md/fix_fragmentation.py
@@ -111,6 +111,103 @@ def fix_frag_free_numba(nat, rxyz, rcovs, threshold = 1.3):
         # belong[:] = False
         # belong[0] = True
 
+def fix_frag_slab(initial_structure: Atoms, threshold = 1.3):
+
+    # get all covalent radii from ASE
+    rcovs = np.empty(len(initial_structure))
+    i = 0
+    for atom in initial_structure:
+        rcovs[i] = covalent_radii[atomic_numbers[atom.symbol]]
+        # print(i, atom.symbol, rcovs[i])
+        i+=1
+
+    nat = len(initial_structure)
+    rxyz = initial_structure.positions
+    return fix_frag_slab_numba(nat, rxyz, rcovs, threshold)
+
+
+
+def fix_frag_slab_numba(nat, rxyz, rcovs, threshold=1.3, constrains_freeze = []):
+    """
+    Fix fragmented slabs by translating fragments in z-direction to bring them closer together.
+
+    Parameters:
+    - nat: int, number of atoms
+    - rxyz: np.ndarray, shape (nat, 3), atomic coordinates
+    - rcovs: np.ndarray, shape (nat,), covalent radii
+    - threshold: float, distance factor for bonding
+    - constrains_freeze: list, indices of atoms to freeze
+
+    Returns:
+    - None (in-place modification of rxyz)
+    """
+    cutoffs = threshold * rcovs
+    max_cut = np.max(cutoffs)
+    min_cut = max_cut / threshold
+    did_fix = False
+
+    # Sort atoms by z to check for gaps
+    zvalues = rxyz[:, 2].copy()
+    sorted_indices = np.argsort(zvalues)
+    z_sorted = zvalues[sorted_indices]
+
+    zdiff = np.diff(z_sorted)
+
+    # If no big gap, return
+    if np.all(zdiff <= max_cut):
+        return
+
+    # Fragmented: identify fragments
+    fragments = np.zeros(nat, dtype=np.int32)
+    current_frag = 0
+    fragments[sorted_indices[0]] = current_frag
+
+    for i in range(1, nat):
+        idx_prev = sorted_indices[i - 1]
+        idx_curr = sorted_indices[i]
+        if abs(rxyz[idx_curr, 2] - rxyz[idx_prev, 2]) <= max_cut:
+            fragments[idx_curr] = current_frag
+        else:
+            current_frag += 1
+            fragments[idx_curr] = current_frag
+
+    # list of fragments that contain at least one atom that should be frozen
+    frozen_frags = []
+    for i_constr in constrains_freeze:
+        frozen_frags.append(fragments[sorted_indices[i_constr]])
+
+
+    # if frozen_frags is not empty
+    if len(frozen_frags) > 0:
+        first_frag = frozen_frags[0]
+        # covert frozen_frags to set
+        frozen_frags = set(frozen_frags)
+        # if more than one fragment frozen, abort.
+        if len(frozen_frags) > 1:
+            print("More than one fragment frozen. Aborting.")
+            quit()
+
+        if first_frag != 0:
+            print("When using slabs, particles with the lowest z values can be frozen.")
+            quit()
+
+    # Move fragments above the first to close gaps
+    for frag_id in range(1, current_frag + 1):
+        did_fix = True
+        frag_mask = fragments == frag_id
+        prev_mask = fragments < frag_id
+
+        # Minimum z of current fragment
+        z_min = np.min(rxyz[frag_mask, 2])
+        # Maximum z of previous fragment(s)
+        z_max_prev = np.max(rxyz[prev_mask, 2])
+
+        # Shift current fragment to close gap
+        dz = z_max_prev + min_cut - z_min
+        rxyz[frag_mask, 2] += dz
+    
+    return did_fix
+
 if __name__ == "__main__":
     import time
     import sys
